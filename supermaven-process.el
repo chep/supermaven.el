cf061ccee880550c60014eeb2f054a493366acf8
Cédric Chépied <cedric.chepied@drotek.com>
2024-05-20 17:23:55 +0200

    Basic completion

--- supermaven-process.el
+++ supermaven-process.el
@@ -1,7 +1,11 @@
-
-(defvar supermaven-process nil)
+(require 'json)
 
 (defconst supermaven-buffer "*supermaven-messages*")
+
+(defvar supermaven-process nil)
+(defvar supermaven-compl-callback nil)
+(defvar supermaven-current-state-id -1)
+(defvar supermaven-current-compl "")
 
 (defun supermaven-filter(process string)
   (let ((buf(get-buffer-create "*supermaven*")))
@@ -10,14 +14,19 @@
 	  (goto-char (point-max))
 	  (insert string)))
 
-  (when (string-prefix-p "SM-MESSAGE" string)
-	(let ((json (json-parse-string (substring string 10))))
-	  (supermaven-process json)))
-)
+  (while (string-match "SM-MESSAGE" string)
+	(let ((offset (string-match "SM-MESSAGE" string))
+		  (next (string-match "SM-MESSAGE" string 10)))
+	  (when offset
+		(supermaven-process (json-parse-string (substring string 11 (if next next (length string))))))
+	  (if next
+		  (setq string (substring string next))
+		(setq string "")))))
 
-(defun supermaven-process-start()
+(defun supermaven-process-start(compl-callback)
   (when supermaven-process
 	(delete-process supermaven-process))
+  (setq supermaven-compl-callback compl-callback)
   (setq supermaven-process (make-process :name "supermaven"
 										 :command '("/home/chep/outils/supermaven/sm-agent" "stdio")
 										 :connection-type 'pipe
@@ -30,9 +39,9 @@
 
 (defun supermaven-process(json)
   (cond ((string= (gethash "kind" json) "response")
-		 (supermaver-update-state-id json))
+		 (supermaven-update-state-id json))
 		((string= (gethash "kind" json) "metadata")
-		 (supermaver-update-metadata json))
+		 (supermaven-update-metadata json))
 		((string= (gethash "kind" json) "activation_request")
 		 (save-excursion
 		   (set-buffer (get-buffer-create supermaven-messages))
@@ -50,9 +59,25 @@
 	(process-send-string supermaven-process (concat message "\n"))
 )
 
-(defun supermaver-update-state-id(json))
+(defun supermaven-update-state-id(json)
+  (let ((state-id (string-to-number (gethash "stateId" json))))
+	(if (= state-id supermaven-current-state-id)
+		(let ((items (gethash "items" json)))
+		  (seq-doseq (item items)
+			(cond ((string= (gethash "kind" item) "text")
+				   (setq supermaven-current-compl (concat supermaven-current-compl (gethash "text" item))))
+				  ((string= (gethash "kind" item) "end")
+				   (when supermaven-compl-callback
+					 (supermaven-compl-callback supermaven-current-compl)
+					 (setq supermaven-current-compl ""
+						   supermaven-current-state-id (1+ supermaven-current-state-id)))))))
+	  (progn
+		(setq supermaven-current-compl ""
+			  supermaven-current-state-id state-id)
+		(supermaven-update-state-id json)))))
 
-(defun supermaver-update-metadata(json))
+
+(defun supermaven-update-metadata(json))
 
 
 (provide 'supermaven-process)
--- supermaven.el
+++ supermaven.el
@@ -2,9 +2,17 @@
 
 (defvar supermaven-next-id 0)
 
+(defvar-local supermaven-overlay nil
+  "Overlay for Supermaven completion.")
+
+(defface supermaven-overlay-face
+  '((t :inherit shadow))
+  "Face for Supermaven overlay.")
+
+
 (defun supermaven-start()
   (interactive)
-  (supermaven-process-start)
+  (supermaven-process-start 'supermaven-compl-callback)
   (setq supermaven-next-id 0))
 
 (defun supermaven-use-free()
@@ -14,24 +22,74 @@
 							   "\n")))
 
 
-(defun supermaven-on-update (buffer)
-  (let ((hash (make-hash-table :test 'equal))
-		(file (make-hash-table :test 'equal))
-		(cursor (make-hash-table :test 'equal)))
+(defun supermaven-on-update (buffer do-send-file)
+  (let* ((hash (make-hash-table :test 'equal))
+		 (updates))
 	(puthash "newId" (number-to-string supermaven-next-id) hash)
 	(setq supermaven-next-id (+ 1 supermaven-next-id))
 	(puthash "kind" "state_update" hash)
 
-	(puthash "kind" "file_update" file)
-	(puthash "path" (buffer-file-name buffer) file)
-	(with-current-buffer buffer
-	  (puthash "content" (buffer-substring-no-properties (point-min) (point-max)) file))
+	;; always cursor
+	(let ((cursor (make-hash-table :test 'equal)))
+	  (puthash "kind" "cursor_update" cursor)
+	  (puthash "path" (buffer-file-name buffer) cursor)
+	  (puthash "offset" (point) cursor)
+	  (setq updates (vector cursor)))
 
-	(puthash "kind" "cursor_update" cursor)
-	(puthash "path" (buffer-file-name buffer) cursor)
-	(puthash "offset" (point) cursor)
+	(when do-send-file
+	  (let ((file (make-hash-table :test 'equal)))
+		(puthash "kind" "file_update" file)
+		(puthash "path" (buffer-file-name buffer) file)
+		(with-current-buffer buffer
+		  (puthash "content" (buffer-substring-no-properties (point-min) (point-max)) file))
+		(setq updates (vconcat updates (vector file)))))
 
-	(puthash "updates" (vector file cursor) hash)
+	(puthash "updates" updates hash)
 
 	(supermaven-send (json-serialize hash))
 	))
+
+(defun supermaven-compl-callback(completion)
+  (supermaven-clear-overlay)
+  (let* ((ov (supermaven-get-overlay))
+		 (tail (buffer-substring (point) (line-end-position)))
+		 (p-completion (concat (propertize completion 'face 'supermaven-overlay-face) tail)))
+	(move-overlay ov (point) (line-end-position))
+
+	(if (eolp)
+        (progn
+          (overlay-put ov 'after-string "") ; make sure posn is correct
+		  (put-text-property 0 1 'cursor t p-completion)
+		  (overlay-put ov 'display "")
+          (overlay-put ov 'after-string p-completion))
+	  (overlay-put ov 'display (substring p-completion 0 1))
+      (overlay-put ov 'after-string (substring p-completion 1)))
+	(overlay-put ov 'completion completion)
+	(overlay-put ov 'start (point))))
+
+
+(defun supermaven-get-overlay ()
+  "Create or get overlay for Supermaven."
+  (unless (overlayp supermaven-overlay)
+    (setq supermaven-overlay (make-overlay 1 1 nil nil t)))
+  supermaven-overlay)
+
+(defun supermaven-clear-overlay ()
+  (when (overlayp supermaven-overlay)
+	(message "clear")
+	(delete-overlay supermaven-overlay)))
+
+(defun supermaven-accept-completion ()
+  (interactive)
+  (let* ((ov (supermaven-get-overlay))
+		 (completion (overlay-get ov 'completion)))
+	(insert completion)
+	(supermaven-clear-overlay)))
+
+(defun supermaven-reject-completion ()
+  (interactive)
+  (supermaven-clear-overlay))
+
+(defun supermaven-complete ()
+  (interactive)
+  (supermaven-on-update (current-buffer) t))
